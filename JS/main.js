56cc5d0fa350f6e6035b759ec37b4344152f7c6b
nemo2109 <114691858+nohemy09@users.noreply.github.com>
2023-01-29 17:32:31 -0600

    Funcionalidad en JS iniciada

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,23 @@
 import '../assets/styles/style.scss';
 
+// Card info
+let card_number = document.querySelector(".card-number");
+let full_name = document.querySelector(".name");
+let exMM = document.getElementById("ex-mm");
+let exYY = document.getElementById("ex-yy");
+let cvcf = document.querySelector(".cvc");
+
+
+// Variables de las inputs
+let Name = document.getElementById("name");
+let cardNumber = document.getElementById("card-number");
+let w_format = document.getElementById("w-format");
+let ex_mm = document.getElementById("mm");
+let ex_yy = document.getElementById("yy");
+let w_blank = document.getElementById("w-blank");
+let cvc = document-getElementById("cvc");
+let w_blankc = document.getElementById("w-blankc");
+
+
 
 
